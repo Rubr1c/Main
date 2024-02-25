2631da6c4a6b6888eea7c099c9287ee7b9b6ba2f
Rubr1c <alizaghloul64@gmail.com>
2024-02-25 06:18:59 +0200

    Coddy daily challenge feb 25 2024

--- Python/CoddyDaily.py
+++ Python/CoddyDaily.py
@@ -4,3 +4,10 @@ def is_symmetric(n):
     string_num = str(n)
     return string_num == string_num[::-1]
 
+#Feb 25 2024
+
+def recycling_robot(trash, categories):
+    for i in categories:
+        if i.lower() == trash.lower():
+            return i
+    return "Not recyclable"
