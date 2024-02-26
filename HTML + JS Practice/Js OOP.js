64d3647447d91623d7ee4829f1fd2400a5e88779
Rubr1c <alizaghloul64@gmail.com>
2024-02-26 22:34:48 +0200

    oop bank system js

--- HTML + JS Practice/Js OOP.js
+++ HTML + JS Practice/Js OOP.js
@@ -161,3 +161,161 @@
 
 // account1.transfer(account2, 100)
 
+
+
+
+class BankAccount {
+    static count = 0;
+    static users = {};
+    
+
+    constructor(fname, lname) {
+        this.fname = fname;
+        this.lname = lname;
+        this.accountInfo = [];
+    }
+
+    checkUserPassCombo(username, password) {
+        return BankAccount.users[username][0] == password;
+    }
+
+    checkUserExist(username) {
+        return BankAccount.users.hasOwnProperty(username);
+    }
+
+    addUserToDB(username, password, accountNumber, balance) {
+        BankAccount.users[username] = [password, accountNumber, balance];
+    }
+
+    addCount() {
+        BankAccount.count++;
+        return BankAccount.count;
+    }
+
+    updateBalance(username, balance) {
+        BankAccount.users[username][2] = balance;
+    }
+
+    makeAccount(username, password) {
+        if (BankAccount.users.hasOwnProperty(username)) {
+            console.log("username is already taken");
+            return;
+        }
+        let includesNumber = false;
+        const specialChar = "!@#$%^&*()-_=+"
+        let includesSpecial = false;
+        for (const char of password) {
+            if (!isNaN(char)) {
+                includesNumber = true;
+            }
+            if (specialChar.includes(char)) {
+                includesSpecial = true;
+            }
+        }
+        if (!includesSpecial) {
+            console.log("add special char to password");
+            return;
+        } 
+        if (!includesNumber) {
+            console.log("add number to password");
+            return;
+        }
+
+
+        if (username.length >= 4 && password.length >= 8) {
+            this.username = username;
+            this.password = password;
+            const count = this.addCount()
+            this.accountNumber = count;
+            this.balance = 0;
+            this.accountInfo.push(this.username);
+            this.accountInfo.push(this.password);
+            this.accountInfo.push(this.accountNumber);
+            this.accountInfo.push(this.balance);
+            this.addUserToDB(username, password, this.accountNumber, this.balance);
+            console.log("Account Created!");
+            console.log("Username: ", this.username);
+            console.log("Account Number: ", this.accountNumber);
+            console.log("Balance: ", this.balance);
+        } else {
+            console.log("username or password is too short ");
+            return;
+        }
+        
+    }
+
+    checkInfo() {
+        console.log("Username: ", this.username);
+        console.log("Account Number: ", this.accountNumber);
+        console.log("Balance: ", this.balance);
+    }
+
+    deposit(ammount) {
+        this.balance += ammount;
+        this.updateBalance(this.username, this.balance);
+        console.log("Deposited: ", ammount);
+        console.log("New Balance: ", this.balance);
+    }
+
+    withdraw(ammount) {
+        if (this.balance >= ammount) {
+            this.balance -= ammount;
+            this.updateBalance(this.username, this.balance);
+            console.log("Withdrawed: ", ammount);
+            console.log("New Balance: ", this.balance);
+        } else {
+            console.log("Not Enough Funds");
+        }
+    }
+
+    transfer(account, ammount) {
+        if (this.balance >= ammount) {
+            this.balance -= ammount;
+            this.updateBalance(this.username, this.balance);
+            console.log("New Balance: ", this.balance);
+            account.deposit(ammount);
+        }
+    }
+
+}
+
+class Login extends BankAccount {
+    constructor(username, password) {
+        super();
+        if (super.checkUserExist(username)) {
+            if (super.checkUserPassCombo(username, password)) {
+                this.username = username;
+                this.password = password;
+                this.accountNumber = BankAccount.users[username][1];
+                this.balance = BankAccount.users[username][2];
+                console.log("Logged in!");
+            } else {
+                console.log("Incorrect password");
+            }
+        } else {
+            console.log("User Does Not Exist");
+        }
+    }
+
+}
+
+
+
+const account1 = new BankAccount("Ali", "Zaghloul");
+
+account1.makeAccount("Rubric", "Rubric22!")
+
+const account2 = new BankAccount("Rubric", "zz");
+
+account2.makeAccount("Choo", "21@!aaaaaa")
+
+const account3 = new Login("Rubric", "Rubric22!");
+
+account2.deposit(100);
+account3.deposit(200);
+
+account2.transfer(account3, 50);
+
+account3.checkInfo()
+
+
