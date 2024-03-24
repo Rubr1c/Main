0af39bf04b33f1aed85855f029a118329a28b8ce
Rubr1c <alizaghloul64@gmail.com>
2024-03-24 23:20:50 +0200

    react

---
+++ React/Card-Components/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return(
+        <div className="card">
+            <img className = "card-image" src="https://via.placeholder.com/150" alt="profile picture" />
+            <h2 className="card-title">Rubric</h2>
+            <p className="card-text">learing js and softroll chagoat</p>
+        </div>
+    );
+}
+
+export default Card;
